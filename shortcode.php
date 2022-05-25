0fa625368761035ee3251eaaa76a89b7048abc3e
ChesterX <50480774+ChesterX@users.noreply.github.com>
2022-05-25 08:26:04 +0300

    fix errors

--- shortcode.php
+++ shortcode.php
@@ -284,8 +284,8 @@ function srp_generate_review_posts( $atts ) {
 			<?php
 				the_post_navigation(
 					array(
-						'prev_text' => '<svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M5 9L1 5L5 1" stroke="#7DCDD4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
-						'next_text' => '<svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M1 9L5 5L1 1" stroke="#7DCDD4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+						'next_text' => '<svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M5 9L1 5L5 1" stroke="#7DCDD4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+						'prev_text' => '<svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M1 9L5 5L1 1" stroke="#7DCDD4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
 					)
 				);
 			?>
--- show-review-posts.php
+++ show-review-posts.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Hapigood reviews plugin
  * Plugin URI:        simpals.com
  * Description:       This is a custom Hapigood plugin for reviews showing
- * Version:           3.0.8
+ * Version:           3.0.9
  * Author:            Simpals Dev
  * Author URI:        simpals.com
  * License:           GPL-2.0+
