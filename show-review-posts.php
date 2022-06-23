c04a85ebf5f0c9f81c6ff2bd5934c50e45cabde1
ChesterX <50480774+ChesterX@users.noreply.github.com>
2022-06-23 09:24:22 +0300

    change colors

--- show-review-posts.php
+++ show-review-posts.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Hapigood reviews plugin
  * Plugin URI:        simpals.com
  * Description:       This is a custom Hapigood plugin for reviews showing
- * Version:           4.0.1
+ * Version:           4.0.2
  * Author:            Simpals Dev
  * Author URI:        simpals.com
  * License:           GPL-2.0+
